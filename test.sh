3e49aa5643f845e1fec885d2e7fa08288eca0ef9
Sk_Aziz_Ali <skazizali.007@gmail.com>
2023-08-31 13:28:53 +0200

    Update test.sh
    More important environment variables

--- test.sh
+++ test.sh
@@ -1,12 +1,17 @@
-
 # CUDA_LAUNCH_BLOCKING=1 HYDRA_FULL_ERROR=1 ./test.sh
 
+# ------------- ENVIRONMENT VARIABLES -------------
 export DATASET=kitti_odometry
-export JOB_NAME=train_pwclonet_mine_odometry_evaluate
-export TRAIN_DIR=./train       # Path to the output models 
+export JOB_NAME=test_pwclonet
+export TRAIN_DIR=./test       # Path to the output models
 
-export IN_CHECPOINT_FILE=/mnt/isilon/melamine/relidar-slam/pyLiDAR_SLAM/Best_Models/mine_kitti_shuffling_e_4_trans_pap/train/checkpoints/best.ckp
+export PYLIDAR_SLAM_PWCLONET_ABS_PATH=/mnt/isilon/melamine/pylidar-slam-pwclonet
+export KITTI360_DATASET=/mnt/isilon/melamine/KITTI-360/heavy_data
+export KITTI_DATASET=/mnt/isilon/melamine/KITTI/dataset
+
+# ------------- Input checkpoint file -------------
+export IN_CHECPOINT_FILE=/mnt/isilon/melamine/pylidar-slam-pwclonet/Best_Models/mine_kitti_shuffling_e_4_trans_pap/train/train/checkpoints/best.ckp
 
 # Launches the Training of PoseNet
 #dataset.num_points=8192 -m pdb 
-python train.py +device=cuda:2 +num_workers=16 +num_epochs=30 dataset=${DATASET} +do_train=False +do_test=True +batch_size=8 +in_checkpoint_file=${IN_CHECPOINT_FILE}
+python train.py +device=cuda:0 +num_workers=16 +num_epochs=30 dataset=${DATASET} +do_train=False +do_test=True +batch_size=4 +in_checkpoint_file=${IN_CHECPOINT_FILE}
